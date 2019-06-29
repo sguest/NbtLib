6fc3f1783bae340abe3655198480910f199d4f9e
Spencer Guest <spencer.b.guest@gmail.com>
2019-06-28 22:24:24 -0500

    Static helper functions

---
+++ Source/NbtLib/NbtConvert.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+
+namespace NbtLib
+{
+    public static class NbtConvert
+    {
+        public static NbtCompoundTag ParseNbtStream(Stream stream)
+        {
+            var parser = new NbtParser();
+            return parser.ParseNbtStream(stream);
+        }
+
+        public static Stream CreateNbtStream(NbtCompoundTag rootTag)
+        {
+            var writer = new NbtWriter();
+            return writer.CreateNbtStream(rootTag);
+        }
+
+        public static Stream CreateNbtStream(NbtCompoundTag rootTag, string rootTagName)
+        {
+            var writer = new NbtWriter();
+            return writer.CreateNbtStream(rootTag, rootTagName);
+        }
+
+        public static Stream CreateUncompressedNbtStream(NbtCompoundTag rootTag)
+        {
+            var writer = new NbtWriter();
+            return writer.CreateUncompressedNbtStream(rootTag);
+        }
+
+        public static Stream CreateUncompressedNbtStream(NbtCompoundTag rootTag, string rootTagName)
+        {
+            var writer = new NbtWriter();
+            return writer.CreateUncompressedNbtStream(rootTag, rootTagName);
+        }
+    }
+}
