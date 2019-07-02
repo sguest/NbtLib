053eca13b3623f36845ee53654289570cd7a89db
Spencer Guest <spencer.b.guest@gmail.com>
2019-07-01 21:33:57 -0500

    Serialize simple properties

---
+++ Source/NbtLib/NbtSerializer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace NbtLib
+{
+    public class NbtSerializer
+    {
+        public Stream SerializeObject(object obj)
+        {
+            var tag = SerializeObjectToTag(obj);
+
+            var writer = new NbtWriter();
+            return writer.CreateNbtStream(tag);
+        }
+
+        public NbtCompoundTag SerializeObjectToTag(object obj)
+        {
+            return (NbtCompoundTag)SerializeTag(obj);
+        }
+
+        private INbtTag SerializeTag(object obj)
+        {
+            var targetType = obj.GetType();
+
+            if (targetType == typeof(int))
+            {
+                return new NbtIntTag((int)obj);
+            }
+            else if(targetType == typeof(string))
+            {
+                return new NbtStringTag(obj.ToString());
+            }
+            else
+            {
+                var tag = new NbtCompoundTag();
+
+                foreach (var propInfo in targetType.GetProperties(BindingFlags.Public | BindingFlags.Instance))
+                {
+                    if(propInfo.CanRead)
+                    {
+                        var name = propInfo.Name;
+                        var value = SerializeTag(propInfo.GetValue(obj));
+                        tag.Add(name, value);
+                    }
+                }
+
+                return tag;
+            }
+        }
+    }
+}
---
+++ Tests/NbtLib.Tests/NbtSerializerTests.cs
@@ -0,0 +1,24 @@
+﻿using NbtLib.Tests.Serialization;
+using Xunit;
+
+namespace NbtLib.Tests
+{
+    public class NbtSerializerTests
+    {
+        [Fact]
+        public void SerializeObjectToTag_ShouldSerializeSimpleObject()
+        {
+            var obj = new SimpleObject
+            {
+                Int5 = 5,
+                StringAbcd = "abcd"
+            };
+
+            var serializer = new NbtSerializer();
+            var tag = serializer.SerializeObjectToTag(obj);
+
+            Assert.Equal(5, ((NbtIntTag)tag["Int5"]).Payload);
+            Assert.Equal("abcd", ((NbtStringTag)tag["StringAbcd"]).Payload);
+        }
+    }
+}
